95a00ef021cce18416322c1e33e2f33aff7a97c0
Liam Anthony Edwards <liam0531@stud.kea.dk>
2019-09-04 14:02:26 +0200

    1first upload

---
+++ JavaScript.js
@@ -0,0 +1,60 @@
+/* 1.fetch data
+
+2. cloning, set content*/
+
+fetch("http://kea-alt-del.dk/t5/api/categories").then(res => res.json()).then(function (data) {
+    data.forEach(buildCategory)
+    getProducts();
+})
+
+function buildCategory(data) {
+    const section = document.createElement("section");
+    const header = document.createElement("h1");
+    header.textContent = data;
+    section.setAttribute("id", data)
+    section.appendChild(header);
+    document.querySelector("main").appendChild(section);
+}
+
+function getProducts() {
+    fetch("http://kea-alt-del.dk/t5/api/productlist").then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        data.forEach(showDish)
+    })
+}
+
+function showDish(dish) {
+    console.log(dish)
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector(".name").textContent = dish.name;
+    copy.querySelector(".price").textContent = dish.price;
+
+    if (dish.discount) {
+        copy.querySelector(".price").classList.add("Thediscount");
+        copy.querySelector(".Thediscount").textContent = Math.round(dish.price - dish.discount / 100 * dish.price)
+
+    } else {
+        copy.querySelector(".discount").remove();
+    }
+
+    if (dish.soldout) {} else {
+        copy.querySelector(".soldOut").style.opacity = "0.5"
+        copy.querySelector(".soldOut").remove()
+    }
+
+    if (dish.discount) {} else {
+        copy.querySelector(".discountTag").remove()
+    }
+
+    copy.querySelector(".shortDescription").textContent = dish.shortdescription;
+
+    copy.querySelector(
+        ".shadow").src = `imgs/medium/${dish.image}-md.jpg`;
+
+
+
+
+    document.querySelector("#" + dish.category).appendChild(copy);
+}
